d342508e8c71b083b3ee32f55b4f56b114c1645e
PatrykDrozdz <pdrozdz@onet.eu>
2016-05-13 18:12:17 +0200

    First/last commit

---
+++ form-sim/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace form_sim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.generator = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.proces = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.clear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.worst = new System.Windows.Forms.RadioButton();
+            this.best = new System.Windows.Forms.RadioButton();
+            this.first = new System.Windows.Forms.RadioButton();
+            this.timerCheck = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // generator
+            // 
+            this.generator.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.generator.Location = new System.Drawing.Point(103, 424);
+            this.generator.Name = "generator";
+            this.generator.Size = new System.Drawing.Size(77, 55);
+            this.generator.TabIndex = 0;
+            this.generator.Text = "generuj";
+            this.generator.UseVisualStyleBackColor = true;
+            this.generator.Click += new System.EventHandler(this.generator_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(7, 493);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "liczba procesów i bloków pamięci:";
+            // 
+            // proces
+            // 
+            this.proces.Location = new System.Drawing.Point(228, 492);
+            this.proces.Name = "proces";
+            this.proces.Size = new System.Drawing.Size(152, 20);
+            this.proces.TabIndex = 6;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.clear.Location = new System.Drawing.Point(186, 424);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(104, 53);
+            this.clear.TabIndex = 9;
+            this.clear.Text = "Wyczyść wszystko";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.worst);
+            this.groupBox1.Controls.Add(this.best);
+            this.groupBox1.Controls.Add(this.first);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.Location = new System.Drawing.Point(296, 424);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 53);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "typy dopasowań";
+            // 
+            // worst
+            // 
+            this.worst.AutoSize = true;
+            this.worst.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.worst.Location = new System.Drawing.Point(176, 20);
+            this.worst.Name = "worst";
+            this.worst.Size = new System.Drawing.Size(75, 21);
+            this.worst.TabIndex = 2;
+            this.worst.TabStop = true;
+            this.worst.Text = "worst-fit";
+            this.worst.UseVisualStyleBackColor = true;
+            // 
+            // best
+            // 
+            this.best.AutoSize = true;
+            this.best.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.best.Location = new System.Drawing.Point(97, 20);
+            this.best.Name = "best";
+            this.best.Size = new System.Drawing.Size(69, 21);
+            this.best.TabIndex = 1;
+            this.best.TabStop = true;
+            this.best.Text = "best-fit";
+            this.best.UseVisualStyleBackColor = true;
+            // 
+            // first
+            // 
+            this.first.AutoSize = true;
+            this.first.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.first.Location = new System.Drawing.Point(19, 20);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(65, 21);
+            this.first.TabIndex = 0;
+            this.first.TabStop = true;
+            this.first.Text = "first-fit";
+            this.first.UseVisualStyleBackColor = true;
+            // 
+            // timerCheck
+            // 
+            this.timerCheck.AutoSize = true;
+            this.timerCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.timerCheck.Location = new System.Drawing.Point(2, 424);
+            this.timerCheck.Name = "timerCheck";
+            this.timerCheck.Size = new System.Drawing.Size(95, 46);
+            this.timerCheck.TabIndex = 11;
+            this.timerCheck.Text = "time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(584, 516);
+            this.Controls.Add(this.timerCheck);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.proces);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.generator);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 554);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 554);
+            this.Name = "Form1";
+            this.Text = "Fit algorithms";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generator;
+        
+        /// //////////////////////////
+    
+        private int a = 0;
+        private int m = 0;
+        private int p = 0;
+        private int ticks = 0;
+        private int temp = 0;
+
+        private string s1;
+        private string s2;
+
+        private int[] proc;
+        private int[] memor;
+        private int[] tabX;
+        private int[] tabY;
+
+        /// /////////////////////
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox proces;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton worst;
+        private System.Windows.Forms.RadioButton best;
+        private System.Windows.Forms.RadioButton first;
+        private System.Windows.Forms.Label timerCheck;
+    }
+}
+
---
+++ form-sim/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_sim
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+    
+        /// ///////////////////
+    
+        private System.Random x = new Random();
+
+       private  System.Drawing.SolidBrush myBrush =
+            new System.Drawing.SolidBrush(System.Drawing.Color.Red);//pędzel rysujący
+        private System.Drawing.SolidBrush eraseBrush =
+            new System.Drawing.SolidBrush(System.Drawing.Color.WhiteSmoke);//pędzel wymazujący
+
+        
+        /// /////////////////////////
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Graphics graf = this.CreateGraphics();
+
+            graf.FillEllipse(myBrush, new Rectangle(tabX[ticks] % 580, tabY[ticks] % 380, 20, 30));
+
+            ticks++;
+
+            if (this.ticks == m)
+            {
+                timer.Stop();
+            }
+
+            this.timerCheck.Text = ticks.ToString();
+           
+        }
+
+        //czysczenie zmiennych
+        private void clear_Click(object sender, EventArgs e)
+        {
+            Graphics graf = this.CreateGraphics();
+
+            for (int i=0; i< m; i++)
+            {
+                for(int j=0; j< p; j++)
+                {
+                    graf.FillEllipse(eraseBrush, new Rectangle(tabX[i] % 600, tabY[j] % 370, 20, 30));
+                }
+            }
+
+            this.a = 0;
+            this.m = 0;
+            this.p = 0;
+            this.proces.Clear();
+            this.ticks = 0;
+            this.temp = 0;
+            for (int i = 0; i < this.proc.Length; i++)
+            {
+                proc[i] = 0;
+                memor[i] = 0;
+                tabX[i] = 0;
+                tabY[i] = 0;
+            }
+            this.timerCheck.Text = "time";
+            
+            timer.Enabled = false;
+            timer.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void generator_Click(object sender, EventArgs e)
+        {
+            this.s1 = proces.Text;
+            this.s2 = proces.Text;
+
+            this.m = Convert.ToInt16(s1);
+            this.p = Convert.ToInt16(s2);
+
+            if (this.m < 0)
+            {
+                this.m = (-1) * this.m;
+            }
+
+            if (this.p < 0)
+            {
+                this.p = (-1) * this.p;
+            }
+
+            if (this.a == 0)
+            {
+
+                proc = new int[this.p];//proces
+                tabY = new int[this.p];
+
+                memor = new int[this.m];//pamiec
+                tabX = new int[this.m];
+
+                for (int i = 0; i < this.p; i++)
+                {
+                    proc[i] = x.Next(30, 450);
+                }
+
+                for (int i = 0; i < this.m; i++)
+                {
+                    memor[i] = x.Next(40, 500);
+                }
+
+                if (this.first.Checked && this.s1 != null && this.s2 != null && this.a == 0)
+                {
+
+                    for (int i = 0; i < this.p; i++)
+                    {
+                        for (int j = 0; j < this.m; j++)
+                        {
+                            if (proc[j] <= memor[i] && proc[j] != 0)
+                            {
+                                memor[i] -= proc[j];
+                                tabX[i] = memor[i];
+                                tabY[j] = proc[j];
+
+                                proc[j] = 0;
+                            }
+                        }
+                    }
+                    a++;
+                }
+
+
+                if (this.best.Checked && this.s1 != null && this.s2 != null && this.a == 0)
+                {
+                    //sortowanie
+                    for (int i = 0; i < this.m; i++)
+                    {
+                        for (int j = 0; j < this.m - 1; j++)
+                        {
+                            if (memor[j] > memor[j + 1])
+                            {
+                                temp = memor[i];
+                                memor[i] = memor[j];
+                                memor[j] = temp;
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < this.p; i++)
+                    {
+                        for (int j = 0; j < this.m; j++)
+                        {
+                            if (proc[j] <= memor[i] && proc[j] != 0)
+                            {
+                                memor[i] -= proc[j];
+                                tabX[i] = memor[i];
+                                tabY[j] = proc[j];
+
+                                proc[j] = 0;
+                            }
+
+                        }
+                    }
+                    a++;
+                }
+
+                if (this.worst.Checked && this.s1 != null && this.s2 != null && this.a == 0)
+                {
+                    //sortowanie
+                    for (int i = 0; i < this.m; i++)
+                    {
+                        for (int j = 0; j < this.m; j++)
+                        {
+                            if (memor[i] < memor[j])
+                            {
+                                temp = memor[i];
+                                memor[i] = memor[j];
+                                memor[j] = temp;
+
+                            }
+                        }
+                    }
+
+
+
+                    for (int i = 0; i < this.p; i++)
+                    {
+                        for (int j = 0; j < this.m; j++)
+                        {
+                            if (proc[j] <= memor[i] && proc[j] != 0)
+                            {
+                                memor[i] -= proc[j];
+                                tabX[i] = memor[i];
+                                tabY[j] = proc[j];
+
+                                proc[j] = 0;
+                            }
+
+                        }
+                    }
+                    a++;
+                }
+            }
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+       
+    }
+}
